f9b7a8502ac8d7cdf8c15bc127d8c4ad8f0eeb4a
Karina Skotnikova <karina.slake.koo@gmail.com>
2024-05-27 07:06:29 +0300

    сайт ком2

---
+++ Web/public_html/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    <section id="main">
+        <a href="reg.php">LET US GUIDE YOUR HOME</a>
+        <p>We've more than 745,000 apartments, place & plot</p>
+        <h1>Find Your Perfect Home</h1>
+        <form action="search.php">
+            <input type="text" placeholder="Enter Name, Keywords...">
+            <button><img src="images/search.svg" alt="search"></button>
+        </form>
+    </section>
+    <section id="sponsors">
+        <p>Thousands of world's leading companies trust Space</p>
+        <article id="sponsors-row">
+            <img src="..images\amazon_spon.svg">
+            <img src="..images\amd_spon.svg">
+            <img src="..images\cisco_spon.svg">
+            <img src="..images\dropcam_spon.svg">
+            <img src="..images\logitech_spon.svg">
+            <img src="..images\spotify_spon.svg">
+        </article>
+    </section>
+</body>
+<script src="scripts/index.js"></script>
+</html>
